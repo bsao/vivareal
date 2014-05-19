294b93632f0fb0a349f4b482687a7c11d2791599
bsao <robson@kimmay.com.br>
2014-05-19 20:33:21 -0300

    adding images model

--- images/models.py
+++ images/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Pics(models.Model):
+class Image(models.Model):
     image = models.ImageField(upload_to='static/photos')
     description = models.TextField()

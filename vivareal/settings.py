bd39bf8d09bb989d9de765e5c04f97c99c678fa8
bsao <robson@kimmay.com.br>
2014-05-19 20:49:18 -0300

    adding base template

--- vivareal/settings.py
+++ vivareal/settings.py
@@ -56,6 +56,8 @@ WSGI_APPLICATION = 'vivareal.wsgi.application'
 
 REST_FRAMEWORK = {}
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
@@ -84,3 +86,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+)
--- vivareal/urls.py
+++ vivareal/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 from images import rest_views
 
@@ -8,6 +9,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^images/$', rest_views.ImagesList.as_view()),
 )
